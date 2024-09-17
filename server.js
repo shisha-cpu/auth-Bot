f2a2f95fdca918997617206da40d337fbcc97a3a
salamus <salamus@yandex.com>
2024-09-17 09:06:33 +0300

    deploy

--- server.js
+++ server.js
@@ -5,30 +5,24 @@ import path from 'path';
 const app = express();
 const port = 3000;
 
-
 mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.weppimj.mongodb.net/authBot?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('DB connected'))
   .catch((err) => console.log('DB error:', err));
-
 
 const userSchema = new mongoose.Schema({
   telegramId: { type: String, unique: true }, 
 });
 const User = mongoose.model('User', userSchema);
 
-
-app.get('/user_id=:id', async (req, res) => {
+app.get('/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-  
     const user = await User.findOne({ telegramId: id });
 
     if (user) {
-      
       res.sendFile(path.resolve('public', 'index.html')); 
     } else {
-     
       res.status(404).send('404 - Пользователь не найден');
     }
   } catch (err) {
@@ -37,9 +31,7 @@ app.get('/user_id=:id', async (req, res) => {
   }
 });
 
-
 app.use(express.static('public'));
-
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
